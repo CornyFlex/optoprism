aed17e163d795f9097bf2e35c2863ae425e423fc
CornyFlex <spyrofireice@gmail.com>
2020-10-31 11:38:03 +0100

    Add vue-browser-geolocation and vue2-google-maps, initialize basic project with map coordinates and user location, save data to localstorage on drag

---
+++ src/components/Map.vue
@@ -0,0 +1,98 @@
+<template>
+    <div>
+        <div class="displayCoordinates" style="display: flex;; align-items: center; justify-content: space-between;">
+            <div>
+                <h1>Your Coordinates:</h1>
+                <p>{{ myCoordinates.lat.toFixed(4) }} Latitude, {{ myCoordinates.lng.toFixed(4) }} Longtitude</p>
+            </div>
+            <div>
+                <h1>Map Coordinates:</h1>
+                <p>{{ mapCoordinates.lat }} Latitude, {{ mapCoordinates.lng }} Longtitude</p>
+            </div>
+        </div>
+        <google-map
+            :center="myCoordinates"
+            :zoom="zoom"
+            style="width:500px; height:300px; margin: 0 auto; margin-top:5%;"
+            ref="mapRef"
+            @dragend="handleDrag"
+        ></google-map>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            map: null,
+            myCoordinates: {
+                lat: 0,
+                lng: 0
+            },
+            zoom: 12
+        }
+    },
+    created() {
+        // does user have saved center? use it instead of default:
+        if(localStorage.center){
+            this.myCoordinates = JSON.parse(localStorage.center);
+        } else {
+            //get coordinates from browser request
+            this.$getLocation({})
+                .then(coordinates => {
+                    this.myCoordinates = coordinates;
+                })
+                .catch(error => alert(error));
+        }
+
+        // does user have saved zoom level? use it instead of default:
+        if(localStorage.zoom) {
+            this.zoom = parseInt(localStorage.zoom);
+        }
+
+    },
+    mounted() {
+        // add map to data object
+        this.$refs.mapRef.$mapPromise.then(map => this.map = map);
+    },
+    methods: {
+        handleDrag() {
+            // get center and zoom level
+            let center = {
+                lat: this.map.getCenter().lat(),
+                lng: this.map.getCenter().lng()
+            };
+            let zoom = this.map.getZoom();
+
+            localStorage.center = JSON.stringify(center);
+            localStorage.zoom = zoom;
+        }
+    },
+    computed: {
+        mapCoordinates(){
+            if(!this.map) {
+                return {
+                     lat: 0,
+                     lng: 0
+                };
+            }
+            return {
+                lat: this.map.getCenter().lat().toFixed(4),
+                lng: this.map.getCenter().lng().toFixed(4)
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.displayCoordinates {
+    padding: 0 15%;
+    margin: 0 auto;
+}
+
+.displayCoordinates h1 {
+    font-size: 25px;
+    font-weight: bold;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
+import VueGeolocation from 'vue-browser-geolocation';
 
+Vue.use(VueGeolocation);
 Vue.use(Buefy)
 Vue.config.productionTip = false
 
+import * as VueGoogleMaps from 'vue2-google-maps';
+Vue.use(VueGoogleMaps, {
+    load: {
+        key: 'AIzaSyCt-2hDJH6w6yw7_VRVDjiyPB-7j8jnuII'
+    },
+    installComponents: false
+});
+
+Vue.component('google-map', VueGoogleMaps.Map);
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: h => h(App),
+}).$mount('#app');
